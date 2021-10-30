2463688f604cd11cf9f09544333698dba9398b49
pointerboy <lukatrbovic@outlook.com>
2021-10-30 23:34:22 +0200

    Fixed local web server CORS blockage

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('http://localhost:3000');
 socket.on('connect', () => {
     console.log(`Connected with id: ${socket.id}`);
 });
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const io = require('socket.io')(3000, {
     cors: {
-        origin: ["http://localhost:8080", "https://admin.socket.io"],
+        origin: '*',
     },
 })
 
